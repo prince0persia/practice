4b0bbd01fe65c9dfad91e979a43a12c98d61b5d6
prince0persia <princexdewan@gmail.com>
2025-04-09 11:10:20 +0900

    Create integers.c

---
+++ integers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+it main(void)
+{
+     int x,y;
+     printf("Enter two integers: ")
+     scanf("%d%d",&x,&y);
+     printf("The result of x==y:%d\n",x==y);
+     printf("The result of x!=y:%d\n",x!=y);
+     printf("Result of x>y:%d\n",x>y);
+     printf("The result of x<y:%d\n",x<y);
+     printf("The result of x>=y:%d\n",x>=y):
+     printf("The result of x<=y:%d\n",x<=y);
+     return 0;
+}
